224c3032d5f3dc93375d69e3a622d63b5cd3f6ae
theusazevedd <matheus_ma02@hotmail.com>
2026-04-16 16:42:40 -0300

    chore: inicializa projeto com Spring Web, JPA, H2 e PostgreSQL

---
+++ order-management-api/src/main/java/com/azevedo/order_management_api/OrderManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.azevedo.order_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderManagementApiApplication.class, args);
+	}
+
+}
---
+++ order-management-api/src/test/java/com/azevedo/order_management_api/OrderManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azevedo.order_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
