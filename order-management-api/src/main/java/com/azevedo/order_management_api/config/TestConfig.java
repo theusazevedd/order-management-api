74269bcb74c6b67965757572d7f3e3f3d1644a56
theusazevedd <matheus_ma02@hotmail.com>
2026-04-17 02:02:57 -0300

    chore: add database seeding for test profile

---
+++ order-management-api/src/main/java/com/azevedo/order_management_api/config/TestConfig.java
@@ -0,0 +1,29 @@
+package com.azevedo.order_management_api.config;
+
+import com.azevedo.order_management_api.entity.UserEntity;
+import com.azevedo.order_management_api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.Arrays;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        UserEntity u1 = new UserEntity(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+        UserEntity u2 = new UserEntity(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+
+        userRepository.saveAll(Arrays.asList(u1, u2));
+
+
+    }
+}
